1b8b2b35fdd197f1fcc0a7ec92986fe78eb70b15
root <root@binfalse.de>
2011-06-26 18:30:43 +0200

    added dual proxy to split a port see http://blog.binfalse.de/2011/06/2-applications-for-1-port/

---
+++ scripts/dual-proxy.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+# see http://binfalse.de
+
+use warnings;
+use strict;
+use Net::Proxy;
+
+# for debugging set verbosity => dumping to stderr
+# Net::Proxy->set_verbosity (1);
+
+my $proxy = Net::Proxy->new (
+	{
+		in =>
+		{
+			# listen on 443
+			type => 'dual', host => '0.0.0.0', port => 123,
+			# if client asks for something direct to port 8080
+			client_first => { type => 'tcp', port => 8080 },
+			# if client waits for greetings direct to port 22
+			server_first => { type => 'tcp', port => 22 },
+			# wait for 2 seconds for questions by clients
+			timeout => 2
+		},
+		# we don't use out...
+		out => { type => 'dummy' }
+	}
+);
+
+$proxy->register ();
+Net::Proxy->mainloop ();
