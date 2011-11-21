3fb5ff525cea13284b13cbc61d7041a2e0405d1b
Martin Scharm <martin@binfalse.de>
2011-11-21 18:47:57 +0100

    updated google reader userscript

--- user-scripts/google_reader_unspace.user.js
+++ user-scripts/google_reader_unspace.user.js
@@ -5,4 +5,4 @@
 // @include        *google*/reader/*
 // ==/UserScript==
 
-GM_addStyle(".collapsed{padding:0 !important;background: none repeat scroll 0 0 #FAFAFA !important;} .entry-actions{padding:0 0 5px 0 !important;} .collapsed .entry-icons,.collapsed .entry-source-title,.collapsed .entry-secondary,.collapsed .entry-date,.collapsed .entry-original{top:0 !important;} #top-bar, #lhn-add-subscription-section, #viewer-header {height: 50px !important;}");
+GM_addStyle(".collapsed{padding:0 !important;background: none repeat scroll 0 0 #FAFAFA !important;} .entry-actions{padding:0 0 5px 0 !important;} #entries.list .entry .collapsed {height: 15px !important;} .collapsed .entry-icons,.collapsed .entry-source-title,.collapsed .entry-title,.collapsed .entry-secondary,.collapsed .entry-date,.collapsed .entry-original{top:0 !important;line-height: 15px !important;} #top-bar, #lhn-add-subscription-section, #viewer-header {height: 50px !important;}#entries.list .collapsed .entry-icons, #entries.list .collapsed .entry-main .entry-original{margin-top: 0;}");
