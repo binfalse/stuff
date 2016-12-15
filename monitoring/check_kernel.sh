e7bee50eaaf4cb0b45b61dba004f422882e39d5f
Martin Scharm <martin@binfalse.de>
2016-12-15 17:57:01 +0100

    improved the kernel check

--- monitoring/check_kernel.sh
+++ monitoring/check_kernel.sh
@@ -14,9 +14,25 @@ source /usr/lib/nagios/plugins/utils.sh
 
 
 current_kernel=$(uname -r)
+latest_kernel=$(find /boot/ -name vmlinuz-* | sort -V | tail -1 | sed 's/.*vmlinuz-//')
 
-latest_kernel=$(find /boot/vmlinuz-* | sort -V | tail -1 | sed 's/.*vmlinuz-//')
+# no uname?
+if [ -z "$current_kernel" ]
+then
+    echo "uname returns empty string?"
+    exit ${STATE_CRITICAL}
+fi
 
+
+# no /boot/vmlinuz-* ?
+if [ -z "$latest_kernel" ]
+then
+    echo "no /boot/vmlinuz-* found.."
+    exit ${STATE_CRITICAL}
+fi
+
+
+# compare the strings
 if [ "$current_kernel" = "$latest_kernel" ]
 then
     echo "running kernel is $current_kernel"
